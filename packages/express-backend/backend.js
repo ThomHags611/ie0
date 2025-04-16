7e6e722be19801f65292abf688ee65a564a2cdde
ThomHags611 <thomashagos611@gmail.com>
2025-04-16 11:21:25 -0700

    post /users now working

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -62,7 +62,7 @@ const users = {
   users["users_list"].find((user) => user["id"] === id);
 
 app.get("/users/:id", (req, res) => {
-  const id = req.params["id"]; //or req.params.id
+  const id = req.params["id"];
   let result = findUserById(id);
   if (result === undefined) {
     res.status(404).send("Resource not found.");
@@ -76,3 +76,14 @@ app.listen(port, () => {
     `Example app listening at http://localhost:${port}`
   );
 });
+
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+  };
+  
+  app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+  });
